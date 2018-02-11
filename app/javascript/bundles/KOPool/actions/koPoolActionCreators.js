fad09fbe6f07b5716f05a9cf8c71db744f71d79a
Jack Collins <BlueSkyFlyer2@gmail.com>
2018-02-11 15:20:20 -0700

    Add fetch and some additional login changes

--- app/javascript/bundles/KOPool/actions/koPoolActionCreators.js
+++ app/javascript/bundles/KOPool/actions/koPoolActionCreators.js
@@ -1,15 +1,74 @@
 /* eslint-disable import/prefer-default-export */
 
-import { KO_POOL_NAME_UPDATE, KO_POOL_LOGIN } from "../constants/koPoolConstants";
+import {
+  KO_POOL_NAME_UPDATE,
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+  USER_LOGIN_FAILURE } from "../constants/koPoolConstants";
 
-export const updateSeason = (text) => ({
-  type: KO_POOL_NAME_UPDATE,
-  text
-});
+function userLoginRequest () {
+  return {type: USER_LOGIN_REQUEST}
+}
 
-export const login = (loginParams) => {
-  console.log("In login method in action creator")
-  return {
-    type: KO_POOL_LOGIN, loginParams
+function userLoginSuccess (user) {
+  return {type: USER_LOGIN_SUCCESS, user}
+}
+
+function userLoginFailure (error) {
+  return {type: USER_LOGIN_FAILURE, error}
+}
+
+export function login (loginParams) {
+  console.log("In login method in action creator", loginParams);
+
+  // The error is here because we are returning a function instead of a plain object
+  // I think we need to add `redux-thunk` https://github.com/gaearon/redux-thunk/issues/146
+  return function (dispatch) {
+    dispatch(userLoginRequest());
+    apiClient.loginUser(loginParams)
+      .then((resp) => { dispatch(userLoginSuccess(loginParams))})
+      .catch((error) => { dispatch(userLoginFailure(error))})
   }
 };
+
+const apiClient = {
+  loadPeople: function () {
+    return {
+      then: function (cb) {
+        setTimeout( () => {
+          cb(JSON.parse(localStorage.people || '[]'))
+        }, 1000);
+      }
+    }
+  },
+
+  loginUser: function (loginParams) {
+    fetch('/sessions', {
+      credentials: 'include', // For CORS https://github.com/github/fetch
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        name: 'Hubot',
+        login: 'hubot',
+      })
+    })
+      .then(checkStatus)
+      .then(parseJSON)
+  }
+}
+
+function checkStatus(response) {
+  if (response.status >= 200 && response.status < 300) {
+    return response
+  } else {
+    var error = new Error(response.statusText)
+    error.response = response
+    throw error
+  }
+}
+
+function parseJSON(response) {
+  return response.json()
+}
--- app/javascript/bundles/KOPool/constants/koPoolConstants.js
+++ app/javascript/bundles/KOPool/constants/koPoolConstants.js
@@ -1,4 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 
 export const KO_POOL_NAME_UPDATE = 'KO_POOL_NAME_UPDATE';
-export const KO_POOL_LOGIN = 'KO_POOL_LOGIN';
+export const USER_LOGIN_REQUEST = 'USER_LOGIN_REQUEST';
+export const USER_LOGIN_SUCCESS = 'USER_LOGIN_SUCCESS';
+export const USER_LOGIN_FAILURE = 'USER_LOGIN_FAILURE';
